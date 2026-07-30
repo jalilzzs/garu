b7851f0179c070d6a3d8b6a8ab4915cd80219908
jalilzzs <fdjxjcfc@gmail.com>
2026-07-30 02:53:47 +0100

    index.js

--- loup-garou 2/server/index.js
+++ loup-garou 2/server/index.js
@@ -47,7 +47,7 @@ const PORT = process.env.PORT || 5000;
 
 async function start() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('Connected to MongoDB');
     server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
   } catch (err) {

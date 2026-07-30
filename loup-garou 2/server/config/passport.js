213d8d891bc6814c36885bf77e437531aa678521
jalilzzs <fdjxjcfc@gmail.com>
2026-07-30 02:07:38 +0100

    passport.js

--- loup-garou 2/server/config/passport.js
+++ loup-garou 2/server/config/passport.js
@@ -22,7 +22,7 @@ async function findOrCreateOAuthUser({ provider, providerId, displayName, email,
   return user;
 }
 
-passpif(process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
+if(process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
   passport.use(
     new GoogleStrategy(
     {
